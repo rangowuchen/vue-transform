da5e8386f7babc3ae74036c62b97b1b1fed4c09e
吴陈 <chen.wu@meditrusthealth.com>
2018-05-15 10:50:25 +0800

    使用vue将word转化成html

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="app">
+		<!--<img src="./assets/logo.png">-->
+		<!--<router-view/>-->
+		<div>
+			
+		</div>
+		<VueUEditor ueditorPath="../../static/ueditor1_4_3_3/" @ready="editorReady"></VueUEditor>
+	</div>
+</template>
+
+<script>
+	import VueUEditor from 'vue-ueditor'
+	export default {
+	  name: 'app',
+	  components: {
+	    VueUEditor
+	  },
+		methods: {
+			editorReady(editorInstance) {
+				editorInstance.setContent('Hello world!<br>编辑初始化内容')
+				editorInstance.addListener('contentChange',() =>{
+					console.log('内容改变 ：',editorInstance.getContent())
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
